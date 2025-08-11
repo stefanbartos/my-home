1ef3c45dd1dfa16d3a5e4e0eb8f271fee43abc62
stefanbartos <stefanbartos@users.noreply.github.com>
2025-08-11 12:21:04 +0000

    deploy: 024a759801343fb62eb65ad5004f3cd137c20dcd

--- .eleventy.js
+++
@@ -1,9 +0,0 @@
-/** @param {import("@11ty/eleventy").UserConfig} eleventyConfig */
-export default function (_) {
-    return {
-        dir: {
-            input: "src",
-            output: "public"
-        }
-    }
-}
--- src/index.md
+++
@@ -1,6 +0,0 @@
----
-layout: default
-title: My home
----
-
-# My home
