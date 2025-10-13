f9f44cba88f48e044b8eabe98a31180627eadff1
Stefan Bartos <stefan.svinciak@gmail.com>
2025-10-13 22:29:53 +0200

    added image plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,24 @@
+import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
+
 /** @param {import("@11ty/eleventy").UserConfig} eleventyConfig */
-export default function (_) {
+export default function (eleventyConfig) {
+    eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
+        // output image formats
+        formats: ["avif", "webp", "jpeg"],
+
+        // output image widths
+        widths: ["auto"],
+
+        // optional, attributes assigned on <img> nodes override these values
+        htmlOptions: {
+            imgAttributes: {
+                loading: "lazy",
+                decoding: "async",
+            },
+            pictureAttributes: {}
+        },
+    });
+
     return {
         dir: {
             input: "src",
